a724b71c53a98451e6a272c89f48e94fc6cc3eda
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-09 08:12:08 -0500

    add useeffect function

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Navbar from "./Nav";
 import './App.css';
 
 function App() {
+  // data state to store the Randomeuser API data. State's initial value is an empty array
+  const [data, setData] = useState([]);
+  // Using useEffect to call the API once mounted and set the data
+  useEffect(() => {
+    (async () => {
+      const result = await axios('https://randomuser.me/api/?results=50');
+      setData(result.data.results);
+      console.log(result.data)
+    })();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
