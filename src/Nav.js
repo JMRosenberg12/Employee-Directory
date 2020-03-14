05e56f0c823d9549f7be6ab77395a7dd1e511f8e
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-14 13:56:30 -0500

    edit

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./styles/Nav.css";
+import "./styles/Header.css";
 import 'bootstrap/dist/css/bootstrap.css'
 function Navbar() {
   return (
-    <nav>
-       <h1> Employee Directory</h1>
+    <nav className="bg-success text-white">
+       <h1 className=".bg-success"> Employee Directory</h1>
        <p> Click on carrots to filter by heading or use the search box to narrow your result.</p>
     </nav>
   );
