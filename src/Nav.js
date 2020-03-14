f0e725263862fc4e276be5e377b4f86fd5f54ea6
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-14 13:56:59 -0500

    edit className

--- src/Nav.js
+++ src/Nav.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function Navbar() {
   return (
     <nav className="bg-success text-white">
-       <h1 className=".bg-success"> Employee Directory</h1>
+       <h1> Employee Directory</h1>
        <p> Click on carrots to filter by heading or use the search box to narrow your result.</p>
     </nav>
   );
