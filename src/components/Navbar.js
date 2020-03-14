91f488ca813631a452f9b4d303d1e84c8fa794c9
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-03-14 13:49:03 -0500

    edit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Search from "./Search";
 
 function NavBar({ handleSearchChange }) {
   return (
-    <nav className="navbar navbar-expand bg-success">
+    <nav className="navbar navbar-expand">
       <div className="navbar-collapse row" id="navbarNav">
         <Search handleSearchChange={handleSearchChange} />
       </div>
